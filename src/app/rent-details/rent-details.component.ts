1cd8523e8821531952c43f69904b00655b5c7ae0
MD IMRAN HABIB <mdimranhabib5@gmail.com>
2021-11-16 17:44:01 +0600

    working on pdf report auto print

--- src/app/rent-details/rent-details.component.ts
+++ src/app/rent-details/rent-details.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { RentDetail } from '../shared/rent-detail.model';
 import { RentDetailService } from '../shared/rent-detail.service';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-rent-details',
@@ -41,6 +45,20 @@ export class RentDetailsComponent implements OnInit {
 
   onDetails(id:number){
     this.service.getRentDetails(id);   
+  }
+
+  showReport(rent:RentDetail){
+    var dd = {
+      content: [
+        'First paragraph',
+        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+      ]
+      
+    }
+    
+
+    pdfMake.createPdf(dd).print();
+   
   }
 
 }
